39e5092171c26a425b5b853d5f07a80b46e63038
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-10-12 19:49:56 -0700

    svg functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,27 @@
 const fs = require('fs');
+const pluginSvgSprite = require("eleventy-plugin-svg-sprite");
+
+const shortcodes = require('./utils/shortcodes.js')
 
 module.exports = function (config) {
   config.setLiquidOptions({
     dynamicPartials: true,
   });
 
+  config.addPlugin(pluginSvgSprite, {
+    path: "./src/assets/icons",
+    svgSpriteShortcode: "iconsprite"
+  })
+
+      // Shortcodes
+  Object.keys(shortcodes).forEach((shortcodeName) => {
+      config.addShortcode(shortcodeName, shortcodes[shortcodeName])
+  })
+
+  config.addWatchTarget('./src/assets')
+
   // Static assets to pass through
-  config.addPassthroughCopy('./src/images');
+  config.addPassthroughCopy('./src/assets/images');
   config.addPassthroughCopy('./src/public');
   config.addPassthroughCopy('./src/styles');
   config.addPassthroughCopy('./src/main.js');
@@ -15,6 +30,8 @@ module.exports = function (config) {
     dir: {
       input: 'src',
       output: '_site',
+      includes: '_includes',
+      data: '_data'
     },
     passthroughFileCopy: true,
     templateFormats: ['html', 'md', 'liquid'],
