4bff8b23e6c1f4dd1cc8de87b1907c05c9584fa6
Aaron Happe <aaronmichaelhappe@gmail.com>
2022-10-16 19:18:54 -0700

    svg and color changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,25 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        hibiscus: {
+          DEFAULT: '#BE276F',
+          50: '#EEAECD',
+          100: '#EB9DC2',
+          200: '#E47BAD',
+          300: '#DD5A98',
+          400: '#D63883',
+          500: '#BE276F',
+          600: '#8F1D54',
+          700: '#611439',
+          800: '#320A1D',
+          900: '#040102',
+        },
+        grey: {
+          14: '#262626',
+          17: '2d2d2d',
+        },
+      },
     },
   },
   variants: {},
